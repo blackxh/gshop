f1635cb66d7c4280b31d07d2fcaae93bbac6a2af
black xie <blackxie@outlook.com>
2020-05-14 16:00:14 +0800

    init shop

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <router-view/>
+    <FooterGuide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+
+  export default {
+    components: {
+      FooterGuide
+    },
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  
+</style>
+
+
+
+
+
+
